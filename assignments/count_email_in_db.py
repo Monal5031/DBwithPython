c9992cd260b16304aeda64c3340c88bd6454ee99
Monal Shadi <monalshadi16@gmail.com>
2017-12-30 03:58:02 +0530

    Add processing code for assignment2 of py4e

---
+++ assignments/count_email_in_db.py
@@ -0,0 +1,50 @@
+import urllib.request, urllib.parse, urllib.error
+import sqlite3
+import sys
+
+
+connection = sqlite3.connect('Assignment2.sqlite')
+cursor = connection.cursor()
+
+url = 'https://www.py4e.com/code3/mbox.txt'
+
+
+def fetch_data():
+	request = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+	url_handle = urllib.request.urlopen(request)
+	# Data we read is not unicode so we need to decode
+	data = url_handle.read().decode()
+	return data
+
+def run_db_queries():
+	cursor.execute('''
+	DROP TABLE IF EXISTS Counts''')
+
+	cursor.execute('''
+	CREATE TABLE Counts (org TEXT, count INTEGER)''')
+
+	data = fetch_data()
+	print('data has arrived')
+	for line in data.split('\n'):
+		print('.', end='', flush=True)
+		if not line.startswith('From: '): continue
+		pieces = line.split()
+		org = pieces[1].split('@')[1]
+		cursor.execute('SELECT count FROM Counts WHERE org = ?', (org,))
+		row = cursor.fetchone()
+		if row is None:
+			cursor.execute('''INSERT INTO Counts (org, count)
+				VALUES (?,1)''', (org,))
+		else:
+			cursor.execute('UPDATE Counts SET count = count + 1 WHERE org = ?', (org,))
+	connection.commit()
+
+	sql_string = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
+
+	for row in cursor.execute(sql_string):
+		print(str(row[0]), row[1])
+
+
+run_db_queries()
+
+cursor.close()
