02d32d506cf28b74716e2f99716220c674851708
Monal Shadi <monalshadi16@gmail.com>
2018-01-04 14:14:13 +0530

    Add assignment 4 of py4e

---
+++ assignments/assignment4/roster.py
@@ -0,0 +1,68 @@
+import json
+import sqlite3
+
+connection = sqlite3.connect('rosterdb.sqlite')
+cursor = connection.cursor()
+
+
+# Setup DB
+cursor.executescript('''
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS Member;
+DROP TABLE IF EXISTS Course;
+
+CREATE TABLE User (
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	name TEXT UNIQUE
+);
+
+CREATE TABLE Course (
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	title TEXT UNIQUE
+);
+
+CREATE TABLE Member  (
+	user_id INTEGER,
+	course_id INTEGER,
+	role  INTEGER,
+	PRIMARY KEY (user_id, course_id)
+);
+''')
+
+file_name = input('Enter file name')
+if len(file_name) < 1:
+	file_name = 'roster_data.json'
+
+# [
+# ["Charley", "sil10", 1],
+# ["Mea", "sil20", 2],
+
+str_data = open(file_name).read()
+json_data = json.loads(str_data)
+
+for entry in json_data:
+	name = entry[0]
+	title = entry[1]
+	role = entry[2]
+
+	print((name, title))
+
+	cursor.execute('''INSERT OR IGNORE INTO User (name)
+		VALUES ( ? )''', (name, ))
+	cursor.execute('SELECT id FROM User WHERE name = ?', (name, ))
+	user_id = cursor.fetchone()[0]
+
+
+	cursor.execute('''INSERT OR IGNORE INTO Course (title)
+		VALUES ( ? )''', (title, ))
+	cursor.execute('SELECT id FROM Course WHERE title = ?', (title, ))
+	course_id = cursor.fetchone()[0]
+
+
+	cursor.execute('''INSERT OR REPLACE INTO Member
+		(user_id, course_id, role) VALUES ( ? , ? , ? )''',
+		( user_id, course_id, role) )
+
+connection.commit()
+
+cursor.close()
